3a41df8fbcd37e3e52af5d5b222c6881727cf0c6
Aaron Omotosho <hert5796@gmail.com>
2020-03-28 16:57:36 +0000

    Switched from config.js to .env for firebase config data

--- src/database/firebase.js
+++ src/database/firebase.js
@@ -1,10 +1,15 @@
-import * as firebase from 'firebase'
-import {config} from './config'
+import * as firebase from 'firebase';
 
-
-if (!firebase.apps.length) {
-  firebase.initializeApp(config);
-}
+if (!firebase.apps.length) firebase.initializeApp({
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+	measurementId: process.env.REACT_APP_MEASUREMENT_ID,
+});
 
 const firestore = firebase.firestore();
 
